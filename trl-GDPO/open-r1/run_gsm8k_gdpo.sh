587c35abd6a843e2bbee7588ff55bcfe5724d2c0
rdesc <rodrigue.de.schaetzen@gmail.com>
2026-03-28 10:55:00 -0400

    temp changes

--- trl-GDPO/open-r1/run_gsm8k_gdpo.sh
+++ trl-GDPO/open-r1/run_gsm8k_gdpo.sh
@@ -1,7 +1,7 @@
 
 
-ACCELERATE_LOG_LEVEL=info \
-    accelerate launch --config_file recipes/accelerate_configs/zero3.yaml \
+WANDB_DIR=/home/mila/d/deschaer/scratch/gdpo-output HF_HUB_OFFLINE=1 CUDA_VISIBLE_DEVICES=1,2,3 ACCELERATE_LOG_LEVEL=info \
+    accelerate launch --config_file recipes/accelerate_configs/zero3_1gpu.yaml \
     src/open_r1/gsm8k.py --config recipes/Qwen2.5-1.5B-Instruct/gdpo_gsm8k/config.yaml \
     --vllm_mode colocate
 
