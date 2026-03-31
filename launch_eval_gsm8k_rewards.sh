09ab70e666f26ff3f9514b547d02c8af19339445
girgisro <roger.girgis@gmail.com>
2026-03-30 23:25:23 -0400

    fixing eval.

--- launch_eval_gsm8k_rewards.sh
+++ launch_eval_gsm8k_rewards.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage:
-#   bash launch_eval_gsm8k_rewards.sh /path/to/checkpoint [tokenizer_name_or_path] [output_dir]
+#   bash launch_eval_gsm8k_rewards.sh /path/to/checkpoint [tokenizer_name_or_path] [output_dir] [num_processes]
 #
 # For the GSM8K launch scripts in this repo, the tokenizer should normally be the
 # base model tokenizer used for training, e.g.:
@@ -15,6 +15,7 @@ OPEN_R1_DIR="$SCRIPT_DIR/trl-GDPO/open-r1"
 CHECKPOINT_PATH=${1:?checkpoint path required}
 TOKENIZER_PATH=${2:-}
 OUTPUT_DIR=${3:-"$SCRIPT_DIR/gsm8k_eval_results/$(basename "$CHECKPOINT_PATH")"}
+NUM_PROCESSES=${4:-1}
 
 module load anaconda/3
 module load cudatoolkit/12.4.0
@@ -38,5 +39,5 @@ if [[ -n "$TOKENIZER_PATH" ]]; then
 fi
 
 HF_HUB_OFFLINE=1 HF_DATASETS_OFFLINE=1 ACCELERATE_LOG_LEVEL=info ACCELERATE_USE_DEEPSPEED=false \
-    accelerate launch --config_file recipes/accelerate_configs/zero3.yaml \
+    accelerate launch --num_processes "$NUM_PROCESSES" \
     src/open_r1/eval_gsm8k_rewards.py "${ARGS[@]}"
